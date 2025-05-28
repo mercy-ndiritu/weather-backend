40408efbf7a93f8d07f08eda85dc72c5dc693db6
mercy-ndiritu <mewanjikundiritu@gmail.com>
2025-05-28 12:54:50 +0300

    Add Vercel frontend URL to CORS middleware for deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173'],
+  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173', 'https://weather-app-frontend-seven-self.vercel.app'],
   credentials: true
 }));
 
